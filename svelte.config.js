5aa03e5268d550369af04ce6d0688c48082f4de6
cr3sp0 <136742333+cr3sp0@users.noreply.github.com>
2024-04-24 17:01:08 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { vitePreprocess } from '@sveltejs/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,9 +12,6 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/around-me-don-toliver' : '',
-        }
 	}
 };
 
